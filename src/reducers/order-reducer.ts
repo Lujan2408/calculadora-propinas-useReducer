b5f290b163d7d77774d1479f04f2f6630181ab7d
Jose David <lujan9495@gmail.com>
2024-05-06 16:09:25 -0500

    Creando el reducer y definiendo las acciones en el type

---
+++ src/reducers/order-reducer.ts
@@ -0,0 +1,53 @@
+import { MenuItem, OrderItem } from "../types";
+
+export type OrderActions =
+    {type: 'add-item', payload: { item: MenuItem }} |
+    {type: 'remove-item', payload: { id: MenuItem['id'] }} |
+    {type: 'place-order'} |
+    {type: 'define-tip', payload: { value: number }} 
+
+export type OrderState = {
+    order: OrderItem[],
+    tip: number
+}
+
+export const initialState : OrderState = {
+    order: [],
+    tip: 0
+}
+
+export const orderReducer = (
+    state: OrderState = initialState, 
+    action: OrderActions
+) => {
+
+    if(action.type === 'add-item') {
+        
+        return {
+            ...state
+        }
+    }
+    
+    if(action.type === 'remove-item') {
+        
+        return {
+            ...state
+        }
+    }
+    
+    if(action.type === 'place-order') {
+        
+        return {
+            ...state
+        }
+    }
+    
+    if(action.type === 'define-tip') {
+        
+        return {
+            ...state
+        }
+    }
+
+    return state
+}
